1c942ec03cd23ddb91ea681e1f12022f112d5977
brdhunga <brdhunga@gmail.com>
2022-10-05 20:07:38 +0000

    todo added

--- ecoProject/settings.py
+++ ecoProject/settings.py
@@ -147,3 +147,4 @@ AUTH_USER_MODEL = 'custom_user.CustomUser'
 AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
 AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
 AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
+
